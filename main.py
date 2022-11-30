49cbe44d85f9a1c20a4aaae2b8bd92e5946af292
Diego Yanez <diego_alex40@hotmail.com>
2022-11-30 13:59:55 -0500

    Ejemplo objeto 1

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("Hola mundo");
-
+class persona:
+    nombre = str
+    edad   = int
+    
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad   = edad
+        
+    def saluda (self, otra_persona):
+        return f'Hola {otra_persona.nombre}, me llamo {self.nombre}.' 
+    
+if __name__ == "__main__":
+    Persona1 = persona("Diego", 33)
+    Persona2 = persona("Carla", 35)
+    
+    print (Persona1.saluda(Persona2))
